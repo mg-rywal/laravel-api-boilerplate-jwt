442937d62cb71100b406737eea86b64e5d1b9c31
VPetar <freeze.988@gmail.com>
2019-03-15 11:54:50 +0100

    Updated dingo and to work with laravel 5.8

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,10 @@
 |
 */
 
-Route::get('reset_password/{token}', ['as' => 'password.reset', function($token)
-{
-    // implement your reset password route here!
-}]);
+// commented until resolved
+//Route::get('reset_password/{token}', ['as' => 'password.reset', function($token)
+//{
+//    // implement your reset password route here!
+//}]);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
